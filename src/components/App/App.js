e7c0d8c5da94c157ff405ed997229ae0d6a5c233
Brandon Alfaro <brandonalfaro20@gmail.com>
2023-04-26 21:14:26 -0500

    edited README and packages

---
+++ src/components/App/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from "react-bootstrap/Button";
+
+function App() {
+  return (
+  <form>
+    <div>
+      <h1>
+      <Button>Click Here</Button>
+      </h1>
+    </div>
+  </form>
+  )
+}
+
+export default App;
