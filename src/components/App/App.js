9436f8a1d48e2e406334fb889ade82309a3e785b
Brandon Alfaro <brandonalfaro20@gmail.com>
2023-11-24 11:57:03 -0600

    Added logic to redirect user to home page if they're not an ADMIN

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import TasksPage from '../Tasks/Tasks';
 import HomePage from '../home-page/home-page';
 import Header from '../header/Header';
@@ -12,31 +12,17 @@ import { CreateEmployee } from '../employees/CreateEmployee';
 import { EmployeeDetails } from '../employees/EmployeeDetails';
 import { getCurrentUserRole, login, logout } from '../LoginPage/AuthService';
 import { TaskDetails } from '../Tasks/TaskDetails';
-import { DataProvider } from '../../utils/DataProvider';
 
 function App() {
 
-  const [role, setRole] = useState(undefined);
+  const [currentUserRole, setCurrentUserRole] = useState(undefined);
+  const role = getCurrentUserRole();
 
   useEffect(() => {
-    const fetchUserRole = async () => {
-      try {
-        const userRole = await getCurrentUserRole();
-        setRole(userRole);
-      } catch (error) {
-        // Handle errors if necessary
-        console.error(error);
+      if (role) {
+        setCurrentUserRole(role);
       }
-    };
-
-    fetchUserRole();
-  }, []); 
-
-  useEffect(() => {
-    // This effect will run whenever the role state changes
-    console.log("User Role changed:", role);
-  }, [role]);
-
+    }, [role]);
 
   return (
     <BrowserRouter>
@@ -48,8 +34,18 @@ function App() {
       <Route exact path='/tasks/:id' element={<TaskDetails />} />
       <Route exact path='/tasks/create' element={<CreateTask />} />
       <Route exact path='/employees' element={<EmployeesPage/>} />
-      <Route exact path='/employees/:email' element={<EmployeeDetails role={role}/>} />
-      <Route exact path='/employees/create' element={<CreateEmployee role={role}/>} />
+      {currentUserRole === 'ADMIN' ? (
+          <>
+            <Route exact path='/employees/:email' element={<EmployeeDetails role={role} />} />
+            <Route exact path='/employees/create' element={<CreateEmployee role={role} />} />
+          </>
+        ) : (
+          // Redirect to the home page if the user is not an ADMIN
+          <>
+          <Route path='/employees/:email' element={<Navigate to="/" />} />
+          <Route path='/employees/create' element={<Navigate to="/" />} />
+          </>
+        )}
     </Routes>
     </BrowserRouter>
   )
